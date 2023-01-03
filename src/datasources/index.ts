a1266144d85c4906227bf905b410b8256b333c80
Muhammad Hilman Bin Ahmad <hilman.ahmad@servedeck.com>
2023-01-04 00:40:50 +0800

    first commit

---
+++ src/datasources/index.ts
@@ -0,0 +1,1 @@
+export * from './mysqllb.datasource';
---
+++ src/models/customer.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Customer extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+  })
+  lastName?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  age: number;
+
+  @property({
+    type: 'boolean',
+    default: true,
+  })
+  active?: boolean;
+
+
+  constructor(data?: Partial<Customer>) {
+    super(data);
+  }
+}
+
+export interface CustomerRelations {
+  // describe navigational properties here
+}
+
+export type CustomerWithRelations = Customer & CustomerRelations;
